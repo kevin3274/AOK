7a779d38f08db62afdf44476bba7cf2dd9787163
mdi-odoo <mdi@tinyerp.com>
2018-03-29 18:49:45 +0530

    Changes in the computation of account move lines based on the payment terms.

--- aok_account/models/account.py
+++ aok_account/models/account.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models
+from dateutil.relativedelta import relativedelta
+
+from odoo import api, fields, models
 
 
 class AccountTax(models.Model):
@@ -13,3 +15,42 @@ class AccountPaymentTerm(models.Model):
     _inherit = "account.payment.term"
 
     one_due_amount = fields.Boolean('Create one due amount', default=True)
+
+    @api.one
+    def compute(self, value, date_ref=False):
+        date_ref = date_ref or fields.Date.today()
+        amount = value
+        sign = value < 0 and -1 or 1
+        result = []
+        if self.env.context.get('currency_id'):
+            currency = self.env['res.currency'].browse(self.env.context['currency_id'])
+        else:
+            currency = self.env.user.company_id.currency_id
+        for line in self.line_ids:
+            if line.value == 'fixed':
+                amt = sign * currency.round(line.value_amount)
+            elif line.value == 'percent':
+                amt = currency.round(value * (line.value_amount / 100.0))
+            elif line.value == 'balance':
+                amt = currency.round(amount)
+            if amt:
+                next_date = fields.Date.from_string(date_ref)
+                if line.option == 'day_after_invoice_date':
+                    next_date += relativedelta(days=line.days)
+                elif line.option == 'fix_day_following_month':
+                    next_first_date = next_date + relativedelta(day=1, months=1)  # Getting 1st of next month
+                    next_date = next_first_date + relativedelta(days=line.days - 1)
+                elif line.option == 'last_day_following_month':
+                    next_date += relativedelta(day=31, months=1)  # Getting last day of next month
+                elif line.option == 'last_day_current_month':
+                    next_date += relativedelta(day=31, months=0)  # Getting last day of next month
+                result.append((fields.Date.to_string(next_date), amt))
+                amount -= amt
+        amount = sum(amt for _, amt in result)
+        dist = currency.round(value - amount)
+        if dist:
+            last_date = result and result[-1][0] or fields.Date.today()
+            result.append((last_date, dist))
+        if self.one_due_amount:
+            return [(result[-1][0], value)]
+        return result
