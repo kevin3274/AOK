33f1f891c040ef092df5b53eda385fb78a8fc974
mdi-odoo <mdi@tinyerp.com>
2018-03-29 16:31:39 +0530

    [IMP] Changes in the Payment Terms.

--- aok_account/models/account.py
+++ aok_account/models/account.py
@@ -7,3 +7,9 @@ class AccountTax(models.Model):
     _inherit = 'account.tax'
 
     discount_account_id = fields.Many2one('account.account', domain=[('deprecated', '=', False)], string='Discount Account', ondelete='restrict')
+
+
+class AccountPaymentTerm(models.Model):
+    _inherit = "account.payment.term"
+
+    one_due_amount = fields.Boolean('Create one due amount', default=True)
