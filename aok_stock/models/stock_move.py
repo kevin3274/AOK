dae3fe6bf86692e76a486f8d65643c92b112f94a
mdi-odoo <mdi@tinyerp.com>
2018-04-06 16:07:40 +0530

    Partner on Stock Move and Stock Move Line.

--- aok_stock/models/stock_move.py
+++ aok_stock/models/stock_move.py
@@ -5,4 +5,4 @@ from odoo import fields, models
 class StockMove(models.Model):
     _inherit = "stock.move"
 
-    partner_id = fields.Many2one(related='picking_id.partner_id', string='Partner')
+    partner_id = fields.Many2one(related='picking_id.partner_id', string='Partner', store=True)
--- aok_stock/models/stock_move_line.py
+++ aok_stock/models/stock_move_line.py
@@ -8,4 +8,4 @@ class StockMoveLine(models.Model):
 
     product_tmpl_id = fields.Many2one('product.template', 'Template')
     dangerous_goods_number = fields.Char(related="product_tmpl_id.dangerous_goods_number", string='Dangerous goods number', store=True)
-    partner_id = fields.Many2one(related='picking_id.partner_id', string='Partner')
+    partner_id = fields.Many2one(related='picking_id.partner_id', string='Partner', store=True)
