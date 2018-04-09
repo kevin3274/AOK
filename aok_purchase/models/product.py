d0d60574783e420138d4db26eda7f5a08a35da22
mdi-odoo <mdi@tinyerp.com>
2018-04-09 16:45:03 +0530

    Added an editable tree view for vendor pricelist.

--- aok_purchase/models/product.py
+++ aok_purchase/models/product.py
@@ -37,7 +37,7 @@ class SupplierInfo(models.Model):
     @api.depends('sim_sales_price', 'total_uom_amount')
     def _compute_margin(self):
         for record in self:
-            record.margin_per = ((record.sim_sales_price - record.total_uom_amount) / record.sim_sales_price) * 100
+            record.margin_per = ((record.sim_sales_price - record.total_uom_amount) / record.sim_sales_price or 1.0) * 100
             record.margin = record.sim_sales_price - record.total_uom_amount
 
 
