c22f63db4f5fc5f2dc16ab079ec04b97dd49179e
skantar <skantar12@gmail.com>
2018-04-10 11:19:21 +0200

    AOK Extension, some new fields
    Tickets:
    AOK-12
    AOK-13
    AOK-14
    AOK-15
    AOK-16
    AOK-18
    AOK-19
    AOK-20
    AOK-21

---
+++ aok_extensions/models/product_documents.py
@@ -0,0 +1,88 @@
+##############################################################################
+#
+# Copyright (c) 2018 - Now Modoolar (http://modoolar.com) All Rights Reserved.
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsibility of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# guarantees and support are strongly advised to contract support@modoolar.com
+#
+##############################################################################
+
+from odoo import models, fields, api
+
+
+class IrAttachment(models.Model):
+    _inherit='ir.attachment'
+
+    external_type = fields.Selection(
+        selection=[
+            ('image', 'Image'),
+            ('video', 'Video'),
+            ('document', 'Document')
+        ],
+        string='Link Type',
+        index=True
+    )
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    # Documents / Internal
+
+    file_attachment_ids = fields.One2many(
+        comodel_name='ir.attachment',
+        inverse_name='res_id',
+        domain=[
+            ('res_model', '=', _inherit),
+            ('type', '=', 'binary')
+        ],
+        string='Attachment'
+    )
+
+    url_attachment_ids = fields.One2many(
+        comodel_name='ir.attachment',
+        inverse_name='res_id',
+        domain=[
+            ('res_model', '=', _inherit),
+            ('type', '=', 'url')
+        ],
+        string='Attachment'
+    )
+
+    # Documents / External
+
+    ext_doc_image_ids = fields.One2many(
+        comodel_name='ir.attachment',
+        inverse_name='res_id',
+        domain=[
+            ('res_model', '=', _inherit),
+            ('type', '=', 'url'),
+            ('external_type', '=', 'image'),
+        ],
+        string='Shop Photos'
+    )
+
+    ext_doc_video_ids = fields.One2many(
+        comodel_name='ir.attachment',
+        inverse_name='res_id',
+        domain=[
+            ('res_model', '=', _inherit),
+            ('type', '=', 'url'),
+            ('external_type', '=', 'video'),
+        ],
+        string='Shop Videos'
+    )
+
+    ext_doc_documents_ids = fields.One2many(
+        comodel_name='ir.attachment',
+        inverse_name='res_id',
+        domain=[
+            ('res_model', '=', _inherit),
+            ('type', '=', 'url'),
+            ('external_type', '=', 'document'),
+        ],
+        string='Shop Documents'
+    )
