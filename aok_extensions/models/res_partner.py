4d33e78f73618510d1aac4ec4503b66cf35526a5
skantar <skantar12@gmail.com>
2018-04-10 11:05:42 +0200

    AOK Extension, some new fields Tickets: AOK-12 AOK-13 AOK-14 AOK-15 AOK-16 AOK-18 AOK-19 AOK-20 AOK-21

---
+++ aok_extensions/models/res_partner.py
@@ -0,0 +1,51 @@
+##############################################################################
+#
+# Copyright (c) 2018 - Now Modoolar (http://modoolar.com) All Rights Reserved.
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsibility of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs
+# End users who are looking for a ready-to-use solution with commercial
+# guarantees and support are strongly advised to contract support@modoolar.com
+#
+##############################################################################
+
+from odoo import models, fields, api
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    send_list = fields.Boolean(
+        string='Send list'
+    )
+
+    no_magazines = fields.Integer(
+        string='No magazines'
+    )
+
+    crm_lead = fields.Char(
+        string='CRM-ID'
+    )
+
+    enc_name = fields.Char(
+        string='Name'
+    )
+
+    enc_lastname = fields.Char(
+        string='Lastname'
+    )
+
+    enc_date = fields.Date(
+        string='Date of Birth'
+    )
+
+    enc_insurance_number = fields.Integer(
+        string='Insurance Number'
+    )
+
+    _sql_constraints = [
+        ('email_uniq', 'unique (email)', "Email address already exists!"),
+    ]
+
+
