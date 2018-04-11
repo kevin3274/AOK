3dc5fbbac2e4ebcf3c3306d06fde42fe154cad2b
mdi-odoo <mdi@tinyerp.com>
2018-04-11 18:00:08 +0530

    Dangerous Products.

--- aok_stock/models/stock_move_line.py
+++ aok_stock/models/stock_move_line.py
@@ -6,6 +6,5 @@ from odoo import fields, models
 class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
-    product_tmpl_id = fields.Many2one('product.template', 'Template')
-    dangerous_goods_number = fields.Char(related="product_tmpl_id.dangerous_goods_number", string='Dangerous goods number', store=True)
+    dangerous_goods_number = fields.Char(related="product_id.product_tmpl_id.dangerous_goods_number", string='Dangerous goods number', store=True)
     partner_id = fields.Many2one(related='picking_id.partner_id', string='Partner', store=True)
