4fc56e51984760b3e67978bc19cc439ee1388780
mdi-odoo <mdi@odoo.com>
2018-05-15 15:46:36 +0530

    Changes in Vendor Invoices.

--- aok_account/models/account.py
+++ aok_account/models/account.py
@@ -2,7 +2,8 @@
 
 from dateutil.relativedelta import relativedelta
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
 
 
 class AccountTax(models.Model):
@@ -241,3 +242,44 @@ class AccountPaymentOrder(models.Model):
                     list1.append((0, 0, {'credit': tax_discount, 'name': 'Payment Tax Discount', 'debit': 0.0, 'partner_id': trf_ml_vals.get('partner_id'), 'date': trf_ml_vals.get('date'), 'account_id': discount_account.id}))
             list1.append((0, 0, {'credit': trf_ml_vals.get('credit') - total_discount, 'name': trf_ml_vals.get('name'), 'debit': 0.0, 'partner_id': trf_ml_vals.get('partner_id'), 'date': trf_ml_vals.get('date'), 'account_id': trf_ml_vals.get('account_id')}))
         return list1
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    @api.multi
+    def post(self):
+        invoice = self._context.get('invoice', False)
+        self._post_validate()
+        for move in self:
+            move.line_ids.create_analytic_lines()
+            if move.name == '/':
+                new_name = False
+                journal = move.journal_id
+
+                if invoice and invoice.move_name and invoice.move_name != '/':
+                    new_name = invoice.move_name
+                else:
+                    if journal.sequence_id:
+                        # If invoice is actually refund and journal has a refund_sequence then use that one or use the regular one
+                        sequence = journal.sequence_id
+                        if invoice and invoice.type in ['out_refund', 'in_refund'] and journal.refund_sequence:
+                            if not journal.refund_sequence_id:
+                                raise UserError(_('Please define a sequence for the credit notes'))
+                            sequence = journal.refund_sequence_id
+
+                        new_name = sequence.with_context(ir_sequence_date=move.date).next_by_id()
+                    else:
+                        raise UserError(_('Please define a sequence on the journal.'))
+
+                if new_name and invoice.type != 'in_invoice':
+                    move.name = new_name
+                if new_name and invoice.type == 'in_invoice':
+                    move.name = invoice.number
+        return self.write({'state': 'posted'})
+
+
+class AccountInvoice(models.Model):
+    _inherit = "account.invoice"
+
+    number = fields.Char(related='move_id.name', store=True, copy=False, readonly=False)
