aa1eb2aafbab0ea9a237da04e251743c872d7c2c
mdi-odoo <mdi@odoo.com>
2018-05-16 11:15:08 +0530

    Changes in Sale Order.

--- aok_account/models/account.py
+++ aok_account/models/account.py
@@ -295,3 +295,14 @@ class ProductProduct(models.Model):
     _inherit = "product.product"
 
     analytic_tag_ids = fields.Many2many("account.analytic.tag", string="Analytic Tags")
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    @api.multi
+    @api.onchange('product_id')
+    def product_id_change(self):
+        result = super(SaleOrderLine, self).product_id_change()
+        self.analytic_tag_ids = self.product_id.analytic_tag_ids
+        return result
