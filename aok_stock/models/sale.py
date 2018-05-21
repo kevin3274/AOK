3602983752a8809181faff98e17356dad01abd4b
mdi-odoo <mdi@odoo.com>
2018-05-21 14:35:42 +0530

    Show warning msg for both type of BOM Product.

--- aok_stock/models/sale.py
+++ aok_stock/models/sale.py
@@ -23,16 +23,17 @@ class SaleOrderLine(models.Model):
             precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
             product = self.product_id.with_context(warehouse=self.order_id.warehouse_id.id)
             product_qty = self.product_uom._compute_quantity(self.product_uom_qty, self.product_id.uom_id)
+            if product.bom_ids:  # and product.bom_ids[:1].type == 'phantom':
+                message = _('You have %s kits of %s available to sell.') % \
+                        (product._get_kit_qty(), product.display_name)
+                warning_mess = {
+                    'title': _('Kits avilability!'),
+                    'message': message
+                }
+                return {'warning': warning_mess}
             if float_compare(product.virtual_available, product_qty, precision_digits=precision) == -1:
                 is_available = self._check_routing()
-                if product.bom_ids and product.bom_ids[:1].type == 'phantom':
-                    message = _('You have %s kits of %s available to sell.') % \
-                            (product.kit_qty, product.display_name)
-                    warning_mess = {
-                        'title': _('Kits avilability!'),
-                        'message': message
-                    }
-                    return {'warning': warning_mess}
+
                 if not is_available:
                     message = _('You plan to sell %s %s but you only have %s %s available in %s warehouse.') % \
                             (self.product_uom_qty, self.product_uom.name, product.virtual_available, product.uom_id.name, self.order_id.warehouse_id.name)
