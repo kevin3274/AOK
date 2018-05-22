722c0ca6754d5492cbd6547469bcc89d3ac5246e
mdi-odoo <mdi@odoo.com>
2018-05-22 16:38:22 +0530

    Fixed the unused imports.

--- aok_purchase/models/product.py
+++ aok_purchase/models/product.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import api, fields, models, _
-from odoo.exceptions import UserError
+from odoo import api, fields, models
 
 
 class ProductSupplierinfoFixedCosts(models.Model):
